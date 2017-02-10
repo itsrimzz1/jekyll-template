da64e573ed62e37e7f5e9ffb62d5f9f04986cc73
itsrimzz1 <rishabh.g@hashedin.com>
2017-02-10 17:42:36 +0530

    milestone 3 - hamburger update

--- _assets/scripts/home.js
+++ _assets/scripts/home.js
@@ -1,1 +1,56 @@
 //Write JS logic here
+if( $('#nav-mobile').css('display')=='none') {
+        console.log('true');
+        is_mobile=True
+    }
+
+    // now i can use is_mobile to run javascript conditionally
+
+    /*if (is_mobile == true) {
+       //$('header').css('display')='none';
+    }
+*/
+
+ var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+ console.log(width);
+if(width < 600) {
+  $("#nav-mobile").css('display', 'flex');
+  $('#nav-desktop').css('display','none');
+}
+else{
+  $('#nav-mobile').css('display','none');
+  $('#nav-desktop').css('display','flex');
+}
+
+var lastWidth = $(window).width();
+
+$(window).resize(function(){
+   if($(window).width()!=lastWidth){
+      //execute code here.
+    if(width < 600) {
+      $("#nav-mobile").css('display', 'flex');
+      $('#nav-desktop').css('display','none');
+    }
+    else{
+      $('#nav-mobile').css('display','none');
+      $('#nav-desktop').css('display','flex');
+    }
+    lastWidth = $(window).width();
+   }
+})
+
+$( ".cross" ).hide();
+$( ".menu" ).hide();
+$( ".hamburger" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".hamburger" ).hide();
+$( ".cross" ).show();
+});
+});
+
+$( ".cross" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".cross" ).hide();
+$( ".hamburger" ).show();
+});
+});
