1fe8199e0a910df7fe317c198eafa90634a2bd86
itsrimzz1 <rishabh.g@hashedin.com>
2017-02-12 21:01:50 +0530

    section 1 pixel perfect update, last commit's suggested changes update, footer pixel perfect update

--- _assets/scripts/home.js
+++ _assets/scripts/home.js
@@ -1,56 +1,47 @@
 //Write JS logic here
-if( $('#nav-mobile').css('display')=='none') {
-        console.log('true');
-        is_mobile=True
+"use strict";
+if ( $('#nav-mobile').css('display') === 'none') {
+        isMobile=True
     }
 
-    // now i can use is_mobile to run javascript conditionally
+    // now i can use isMobile to run javascript conditionally
 
-    /*if (is_mobile == true) {
-       //$('header').css('display')='none';
-    }
-*/
-
- var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
- console.log(width);
-if(width < 600) {
+var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+if (width < 600) {
   $("#nav-mobile").css('display', 'flex');
-  $('#nav-desktop').css('display','none');
-}
-else{
-  $('#nav-mobile').css('display','none');
-  $('#nav-desktop').css('display','flex');
+  $('#nav-desktop').css('display', 'none');
+} else {
+  $('#nav-mobile').css('display', 'none');
+  $('#nav-desktop').css('display', 'flex');
 }
 
 var lastWidth = $(window).width();
 
-$(window).resize(function(){
-   if($(window).width()!=lastWidth){
-      //execute code here.
-    if(width < 600) {
-      $("#nav-mobile").css('display', 'flex');
-      $('#nav-desktop').css('display','none');
+$(window).resize(function (){
+    if ($(window).width() !== lastWidth){
+        if (width < 600) {
+          $("#nav-mobile").css('display', 'flex');
+          $('#nav-desktop').css('display', 'none');
+        } else {
+          $('#nav-mobile').css('display', 'none');
+          $('#nav-desktop').css('display', 'flex');
+        }
+        lastWidth = $(window).width();
     }
-    else{
-      $('#nav-mobile').css('display','none');
-      $('#nav-desktop').css('display','flex');
-    }
-    lastWidth = $(window).width();
-   }
-})
+});
 
-$( ".cross" ).hide();
-$( ".menu" ).hide();
-$( ".hamburger" ).click(function() {
-$( ".menu" ).slideToggle( "slow", function() {
-$( ".hamburger" ).hide();
-$( ".cross" ).show();
+$(".cross").hide();
+$(".menu").hide();
+$(".hamburger").click(function () {
+$(".menu").slideToggle("slow", function () {
+$(".hamburger").hide();
+$(".cross").show();
 });
 });
 
-$( ".cross" ).click(function() {
-$( ".menu" ).slideToggle( "slow", function() {
-$( ".cross" ).hide();
-$( ".hamburger" ).show();
-});
+$(".cross").click(function () {
+    $(".menu").slideToggle("slow", function () {
+      $(".cross").hide();
+      $(".hamburger").show();
+    });
 });
