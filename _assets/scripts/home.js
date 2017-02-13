1a5f2c2d9d95639ce4a1f2d1bb802ce485ee92d8
itsrimzz1 <rishabh.g@hashedin.com>
2017-02-13 18:02:25 +0530

    Interactions added. upto milestone 8 completed. milestone 9 in progress. milestone 10 completed

--- _assets/scripts/home.js
+++ _assets/scripts/home.js
@@ -45,3 +45,23 @@ $(".cross").click(function () {
       $(".hamburger").show();
     });
 });
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+
+        window.location.hash = hash;
+      });
+    }
+  });
+});
